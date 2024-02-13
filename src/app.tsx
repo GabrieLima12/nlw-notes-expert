e87bc465e391328c4579839c17fef9ce59e0df96
GabrieLima12 <gabrieldocouto.1207@gmail.com>
2024-02-13 20:35:14 -0300

    feat: adicionando funcionalidade criar notas

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card'
 
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
+
 export function App() {
+
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
+
+    setNotes([newNote, ...notes]);
+  };
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6" >
       <img src={logo} alt="NLW Expert" />
@@ -19,17 +39,11 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         
-        <NewNoteCard />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
         
-        <NoteCard note={{
-          date: new Date(),
-          content: "Hello World"
-        }} />
-
-        <NoteCard note={{
-          date: new Date(2023, 5, 2),
-          content: "Hello World"
-        }} />
+        {notes.map(note => {
+            return <NoteCard key={note.id} note={note} />
+        })}
 
       </div>
     </div>
