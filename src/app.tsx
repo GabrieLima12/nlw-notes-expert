6503598b656d5f6cd6f52f5cc4083d6fe515136c
GabrieLima12 <gabrieldocouto.1207@gmail.com>
2024-02-13 20:55:55 -0300

    feat: adicionando filtro de busca pelo conteúdo da nota

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card'
@@ -11,12 +11,13 @@ interface Note {
 
 export function App() {
 
+  const [search, setSearch] = useState('');
   const [notes, setNotes] = useState<Note[]>(() => {
     const notesOnStorage = localStorage.getItem('notes');
 
     if (notesOnStorage) {
       return JSON.parse(notesOnStorage);
-    }
+    };
 
     return []
   });
@@ -35,6 +36,16 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(newArray));
   };
 
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+
+    setSearch(query);
+  };
+
+  const filteredNotes = search !== '' 
+    ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    : notes;
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6" >
       <img src={logo} alt="NLW Expert" />
@@ -42,7 +53,8 @@ export function App() {
       <form className="w-full">
         <input 
           type="text" 
-          placeholder="Busque em suas notas..." 
+          placeholder="Busque em suas notas..."
+          onChange={handleSearch}
           className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
         />
       </form>
@@ -53,7 +65,7 @@ export function App() {
         
         <NewNoteCard onNoteCreated={onNoteCreated} />
         
-        {notes.map(note => {
+        {filteredNotes.map(note => {
             return <NoteCard key={note.id} note={note} />
         })}
 
